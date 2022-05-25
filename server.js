2cb2172948100d036b8722cf1fbf39458086c0e4
NG YONG SHENG <gnehsheng@gmail.com>
2022-05-25 11:16:42 +0800

    added cors config

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ const io = new Server(server, {
     origin:'*',
     methods: ["GET", "POST"],
     transports: ['websocket', 'polling'],
-  },
-  allowEIO3: true
+  }
 })
 
 io.on("connect", (socket) => {
